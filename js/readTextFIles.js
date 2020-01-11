20bd0a2eb1fff7a1b41040bde8f6321f5f0e6c9d
Nathan Everett <nathan.everett@students.plymouth.ac.uk>
2020-01-11 22:41:34 +0000

    JS ediit
    Added text files

--- js/readTextFIles.js
+++ js/readTextFIles.js
@@ -4,14 +4,4 @@
 
 function loadFile(fileName, writeLocation){
 
-    let rawFile = new XMLHttpRequest();
-    rawFile.open("GET", fileName, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4) {
-            let allText = rawFile.responseText;
-            document.getElementById(writeLocation).innerHTML = allText;
-        }
-    }
-
-
 }
