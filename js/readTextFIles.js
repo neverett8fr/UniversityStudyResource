f291b1d98dc56608dfa96ffcb98a7cc976e35f97
Nathan Everett <nathan.everett@students.plymouth.ac.uk>
2020-01-11 21:24:36 +0000

    JS ediit
    Added text files

--- js/readTextFIles.js
+++ js/readTextFIles.js
@@ -1,2 +1,17 @@
 //hopefully should read in text files and display them on page
 //called when module info is clicked
+
+
+function loadFile(fileName, writeLocation){
+
+    let rawFile = new XMLHttpRequest();
+    rawFile.open("GET", fileName, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4) {
+            let allText = rawFile.responseText;
+            document.getElementById(writeLocation).innerHTML = allText;
+        }
+    }
+
+
+}
